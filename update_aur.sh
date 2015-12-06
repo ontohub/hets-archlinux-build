7ffd758d94ea1d56982b884e8d08d8b511d05972
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-12-06 16:20:58 +0100

    Add functions to upload to the AUR.

---
+++ update_aur.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./create_updated_package.sh
+./update_pkgbuild_and_upload.sh
--- update_pkgbuild_and_upload.sh
+++ update_pkgbuild_and_upload.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 destination="${1:-uni:/home/wwwuser/eugenk/archlinux-aur/hets}"
+PKGREL="${PKGREL:-1}"
 base_dir="$(dirname $0)"
 cd $base_dir
+PKGBUILD="$base_dir/hets-pkg-files/PKGBUILD"
+
+REPOSITORY_URL="https://aur.archlinux.org/hets.git"
+PACKAGE_REPOSITORY="$base_dir/hets-package-repository"
 
 sync_to_remote() {
-  rsync -avhP $(ls build/*.tar.gz) $destination
+  rsync -avhP $PKGBUILD $(ls build/*.tar.gz) $destination
 }
 
 get_version() {
@@ -17,10 +22,9 @@ get_shasum() {
 }
 
 edit_pkgbuild() {
-  PKGBUILD="hets-pkg-files/PKGBUILD"
   ver=$1
   sha=$2
-  if [ -z "$(which gsed)" ]; then
+  if [ -z "$(which gsed 2> /dev/null)" ]; then
     sed -i "s/pkgver=.*/pkgver=$ver/g" $PKGBUILD
     sed -i "s/sha1sums=.*/sha1sums=('$sha')/g" $PKGBUILD
   else
@@ -29,7 +33,50 @@ edit_pkgbuild() {
   fi
 }
 
+sync_repository() {
+  if [ -d "$PACKAGE_REPOSITORY" ]
+  then
+    cd $PACKAGE_REPOSITORY
+    git fetch
+    git reset --hard origin/master
+    cd -
+  else
+    git clone $REPOSITORY_URL $PACKAGE_REPOSITORY
+  fi
+}
+
+install_mksrcinfo() {
+  if !(hash mksrcinfo 2>/dev/null)
+  then
+    echo ""
+    echo "mksrcinfo not installed."
+    echo "Installing pkgbuild-introspection for mksrcinfo."
+    sudo pacman -S pkgbuild-introspection
+  fi
+}
+
+commit() {
+  pkgver=$1
+  pkgrel="${2:-1}"
+
+  cd $PACKAGE_REPOSITORY
+  mksrcinfo
+  git add PKGBUILD .SRCINFO
+  git commit -m "Update to $pkgver-$pkgrel"
+  git push
+  cd -
+}
+
+manage_package_repository() {
+  pkgver=$1
+  sync_repository
+  cp $PKGBUILD $PACKAGE_REPOSITORY
+  install_mksrcinfo
+  commit $pkgver
+}
+
 version=$(get_version)
 shasum=$(get_shasum)
 edit_pkgbuild $version $shasum
 sync_to_remote
+manage_package_repository $version $PKGREL
